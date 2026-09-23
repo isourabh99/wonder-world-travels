914d9c983c89e2bed3dc141b381ddb3b26f83ee0
isourabh99 <isourabhsoni99@gmail.com>
2026-09-23 19:05:33 +0530

    feat: add ExclusiveBlogs component with persona-based travel story filtering

--- src/components/home/exclusive-blogs/ExclusiveBlogs.tsx
+++ src/components/home/exclusive-blogs/ExclusiveBlogs.tsx
@@ -179,9 +179,9 @@ export const ExclusiveBlogs: React.FC = () => {
   };
 
   return (
-    <section className="relative w-full overflow-hidden bg-white pt-10 pb-0">
+    <section className="relative w-full overflow-hidden bg-white pt-10 pb-0 ">
       {/* Outer arched dark blue container */}
-      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-8xl mx-auto ">
         <div className="relative rounded-t-[36px] sm:rounded-t-[48px] border-t-2 border-blue-400/40 border-x border-blue-500/20 bg-gradient-to-b from-[#071d3a] via-[#0a2b55] to-[#041226] pt-10 sm:pt-14 pb-12 shadow-2xl overflow-hidden">
           
           {/* Inner Dashed Border framing */}
