5cbdf1a1e15f99550fb4562af91c51df837bd571
isourabh99 <isourabhsoni99@gmail.com>
2026-09-24 13:33:12 +0530

    chore: configure static export and unoptimized images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'i0.wp.com',
+        protocol: "https",
+        hostname: "i0.wp.com",
       },
       {
-        protocol: 'https',
-        hostname: '**.wp.com',
+        protocol: "https",
+        hostname: "**.wp.com",
       },
       {
-        protocol: 'https',
-        hostname: 'www.wonderworldtravels.com',
+        protocol: "https",
+        hostname: "www.wonderworldtravels.com",
       },
     ],
     qualities: [75, 90],
   },
-  // Add other config options here if needed
 };
 
 export default nextConfig;
